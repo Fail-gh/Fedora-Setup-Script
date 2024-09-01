2fc4e0c3b9f2a6d7005bbcca95e9dc600bf458a7
Fail-gh <matteo.locorotondo@gmail.com>
2024-09-01 15:25:23 +0200

    Refactoring setup.sh: - Removed sudo requirements - No more required specific directory - Update applied offline - The script persists through reboots until done

---
+++ setup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#Set current folder to autostart
+sed -i "s|HOME|$PWD|g" "./autostart/fedora-setup.desktop"
+sed -i "s|HOME|$PWD|g" "./autostart/rpmfusion-setup"
+sed -i "s|HOME|$PWD|g" "./autostart/nvidia-secure-boot"
+sed -i "s|HOME|$PWD|g" "./autostart/user-configuration"
+
+#Create autostart folder as user
+mkdir -p $HOME/.config/autostart
+
+cp ./autostart/* $HOME/.config/autostart/
+
+#Garant execution permission to all scripts
+chmod +x ./setup-rpmfusion.sh
+chmod +x ./btrfs-maintenance-configuration.sh
+chmod +x ./nvidia-secure-boot.sh
+chmod +x ./user-configuration.sh
+
+#Update system
+dnf clean all
+pkcon refresh force
+while [ $? != 0 ]
+do
+	echo -e "\nRetrying in 5 seconds...\n"
+	sleep 5
+	pkcon refresh force
+done
+pkcon update --only-download
+while [[ $? != 0 && $? != 5 ]]
+do
+	echo -e "\nRetrying in 5 seconds...\n"
+	sleep 5
+	pkcon update --only-download
+done
+if [ $? == 0 ]
+then
+	pkcon offline-trigger
+fi
+
+rm $HOME/.config/autostart/fedora-setup.desktop
+mv $HOME/.config/autostart/setup-rpmfusion $HOME/.config/autostart/setup-rpmfusion.desktop
+
+reboot
