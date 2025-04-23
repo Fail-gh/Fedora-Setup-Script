e2f6c49723fe551c81460d3962e45bad1ab2afb1
Fail-gh <matteo.locorotondo@gmail.com>
2025-04-23 14:38:23 +0200

    Merged all functions in one

--- setup.sh
+++ setup.sh
@@ -1,39 +1,13 @@
 #!/bin/bash
 
-dnf-install () {
-	sudo dnf install -y $1 $2
+dnf-manager () {
+	sudo dnf $1 -y $2 $3
 
 	while [ $? -ne 0 ]
 	do
 		echo -e "\nRetrying in 5 seconds...\n"
 		sleep 5
-		sudo dnf install -y $1 $2
-	done
-
-	echo
-}
-
-dnf-swap () {
-	sudo dnf swap -y $1 $2
-
-	while [ $? -ne 0 ]
-	do
-		echo -e "\nRetrying in 5 seconds...\n"
-		sleep 5
-		sudo dnf swap -y $1 $2
-	done
-
-	echo
-}
-
-dnf-upgrade () {
-	sudo dnf upgrade -y $1 $2 $3
-
-	while [ $? -ne 0 ]
-	do
-		echo -e "\nRetrying in 5 seconds...\n"
-		sleep 5
-		sudo dnf upgrade -y $1 $2 $3
+		sudo dnf $1 -y $2 $3
 	done
 
 	echo
@@ -52,21 +26,8 @@ flatpak-install () {
 	echo
 }
 
-dnf-remove () {
-	sudo dnf remove -y $1
-
-	while [ $? -ne 0 ]
-	do
-		echo -e "\nRetrying in 5 seconds...\n"
-		sleep 5
-		sudo dnf remove -y $1
-	done
-
-	echo -en "\n"
-}
-
 # Install BTRFS Assistant for GUI BTRFS management
-dnf-install "btrfs-assistant"
+dnf-manager "install" "btrfs-assistant"
 
 # Auto BTRFS maintenance configuration
 sudo sed -i 's|BTRFS_BALANCE_MOUNTPOINTS="/"|BTRFS_BALANCE_MOUNTPOINTS="/:/home"|g' "/etc/sysconfig/btrfsmaintenance"
@@ -85,7 +46,7 @@ sudo systemctl enable snapper-cleanup.timer
 echo
 
 # Install SELinux Troubleshooter to analyze and resolve AVC denials
-dnf-install "setroubleshoot"
+dnf-manager "install" "setroubleshoot"
 
 # Enable Fedora Third-Party Repositories
 sudo fedora-third-party enable
@@ -100,51 +61,51 @@ sudo sed -i 's|enabled=1|enabled=0|g' "/etc/yum.repos.d/rpmfusion-nonfree-nvidia
 sudo flatpak remote-modify --disable fedora
 
 # Add RPMFusion repositories
-dnf-install "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+dnf-manager "install" "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 
 # Enable users to install packages using Gnome Software or similar (Only GUI packages)
-dnf-upgrade "@core"
+dnf-manager "upgrade" "@core"
 
 # Allows the application using the gstreamer framework and other multimedia software, to play others restricted codecs
-dnf-upgrade "--setopt=install_weak_deps=False" "--exclude=PackageKit-gstreamer-plugin" "@multimedia"
+dnf-manager "upgrade" "--setopt=install_weak_deps=False" "--exclude=PackageKit-gstreamer-plugin" "@multimedia"
 
 # Install Hardware Accelerated Codec for Intel (Use libva-intel-driver for Haswell, 4 gen, 2013 or older)
-dnf-install "intel-media-driver"
+dnf-manager "install" "intel-media-driver"
 
 # Install RPMOther exit codes could be returned by the specific command itself, see its documentation for Fusion Free Tainted repo
-dnf-install "rpmfusion-free-release-tainted"
-dnf-install "libdvdcss"
+dnf-manager "install" "rpmfusion-free-release-tainted"
+dnf-manager "install" "libdvdcss"
 
 # Install RPMFusion NonFree Tainted repo
-dnf-install "rpmfusion-nonfree-release-tainted"
-dnf-install "--repo=rpmfusion-nonfree-tainted" "*-firmware"
+dnf-manager "install" "rpmfusion-nonfree-release-tainted"
+dnf-manager "install" "--repo=rpmfusion-nonfree-tainted" "*-firmware"
 
 # NVIDIA driver installation if NVIDIA hardware is detected
 nvidia=$(lspci | grep NVIDIA)
 
 if [ -n "$nvidia" ]
 then
-	dnf-install "akmod-nvidia xorg-x11-drv-nvidia-cuda libva-nvidia-driver.i686 libva-nvidia-driver.x86_64"
+	dnf-manager "install" "akmod-nvidia xorg-x11-drv-nvidia-cuda libva-nvidia-driver.i686 libva-nvidia-driver.x86_64"
 fi
 
 # Switch to full ffpmeg
-dnf-swap "--allowerasing" "ffmpeg-free ffmpeg"
+dnf-manager "swap" "--allowerasing" "ffmpeg-free ffmpeg"
 
 # Install mesa Hardware Accelerated Codec
-dnf-swap "mesa-va-drivers mesa-va-drivers-freeworld"
-dnf-swap "mesa-vdpau-drivers mesa-vdpau-drivers-freeworld"
-dnf-swap "mesa-va-drivers.i686 mesa-va-drivers-freeworld.i686"
-dnf-swap "mesa-vdpau-drivers.i686 mesa-vdpau-drivers-freeworld.i686"
+dnf-manager "swap" "mesa-va-drivers mesa-va-drivers-freeworld"
+dnf-manager "swap" "mesa-vdpau-drivers mesa-vdpau-drivers-freeworld"
+dnf-manager "swap" "mesa-va-drivers.i686 mesa-va-drivers-freeworld.i686"
+dnf-manager "swap" "mesa-vdpau-drivers.i686 mesa-vdpau-drivers-freeworld.i686"
 
 # Replace Rhythmbox with GNOME default apps
-dnf-remove "rhythmbox"
-dnf-install "decibels gnome-music"
+dnf-manager "remove" "rhythmbox"
+dnf-manager "install" "decibels gnome-music"
 
 # Install AppIndicator and KStatusNotifierItem Support
-dnf-install "gnome-shell-extension-appindicator"
+dnf-manager "install" "gnome-shell-extension-appindicator"
 
 # Replace RPMs with Flatpaks
-dnf-remove "mediawriter gnome-boxes @libreoffice firefox"
+dnf-manager "remove" "mediawriter gnome-boxes @libreoffice firefox"
 rm -r $HOME/.mozilla
 flatpak-install "org.fedoraproject.MediaWriter org.gnome.Boxes org.libreoffice.LibreOffice org.mozilla.firefox"
 
