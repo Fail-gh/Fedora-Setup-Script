004c171d08599a3fb23014f16cbee26887195c00
Fail-gh <matteo.locorotondo@gmail.com>
2025-04-28 17:42:21 +0200

    Added optional package

--- setup.sh
+++ setup.sh
@@ -97,7 +97,7 @@ then
 	dnf-manager "install" "intel-compute-runtime"
 fi
 
-# Install ROCm runtime if AMD gpu is detected *NOT WORKING (DEPENDENCY ERROR)*
+# Install ROCm runtime if AMD gpu is detected *NOT WORKING (DEPENDENCY ERROR)* *optional package* amdsmi, rocminfo, rocm-clinfo
 amd_gpu=$(lspci | grep VGA | grep AMD)
 
 if [ -n "$amd_gpu" ]
