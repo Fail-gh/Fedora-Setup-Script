beb3d608120abdadbebdbfc591ccb730511d6f3b
Fail-2K <matteo.locorotondo@ikmail.com>
2026-03-14 15:14:36 +0100

    Update for Fedora 44

--- setup.sh
+++ setup.sh
@@ -132,7 +132,7 @@ echo
 if [ "$XDG_SESSION_DESKTOP" == "gnome" ]
 then
 	# Move SELinux Troubleshooter to "System" folder
-	gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/System/ apps "['org.gnome.baobab.desktop', 'org.gnome.DiskUtility.desktop', 'org.gnome.Logs.desktop', 'org.freedesktop.MalcontentControl.desktop', 'org.freedesktop.GnomeAbrt.desktop', 'setroubleshoot.desktop', 'org.gnome.SystemMonitor.desktop']"
+	gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/System/ apps "['org.gnome.baobab.desktop', 'org.gnome.DiskUtility.desktop', 'org.gnome.Logs.desktop', 'org.freedesktop.MalcontentControl.desktop', 'setroubleshoot.desktop', 'org.gnome.SystemMonitor.desktop']"
 fi
 
 # Remove RPMFusion setup from autostart
